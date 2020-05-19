850a13e1b6318881211db6cf9dee2644096d516b
Yuriy <ternxstyle@gmail.com>
2020-05-20 00:48:26 +0300

    add 3 task

--- module-03/tasks/task-03.js
+++ module-03/tasks/task-03.js
@@ -1,1 +1,45 @@
 'use strict';
+
+const findBestEmployee = function (employees) {
+  const employeesKeys = Object.values(employees);
+  let max = employeesKeys[0]; 
+  
+  for (let i = 1; i < employeesKeys.length; i += 1) {
+    if (max < employeesKeys[i]) {
+      max = employeesKeys[i];
+    }
+  }
+
+  const maxEmployee = employeesKeys.indexOf(max);  
+  const value = Object.keys(employees);
+   
+  let summary = value[maxEmployee];
+
+  return summary;
+};
+
+console.log(
+  findBestEmployee({
+    ann: 29,
+    david: 35,
+    helen: 1,
+    lorence: 99,
+  }),
+); // lorence
+
+console.log(
+  findBestEmployee({
+    poly: 12,
+    mango: 17,
+    ajax: 4,
+  }),
+); // mango
+
+console.log(
+  findBestEmployee({
+    lux: 147,
+    david: 21,
+    kiwi: 19,
+    chelsy: 38,
+  }),
+); // lux
