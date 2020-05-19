60f24e6474495bebbce100ac3ca4ecb300e80f9d
Yuriy <ternxstyle@gmail.com>
2020-05-20 01:53:41 +0300

    add 4 task

--- module-03/tasks/task-04.js
+++ module-03/tasks/task-04.js
@@ -1,2 +1,29 @@
 'use strict';
 
+const countTotalSalary = function (employees) {
+  const salariesSum = Object.values(employees);
+  let sum = 0;
+  
+  for (const value of salariesSum) {
+    sum += value;
+  }
+  return sum;
+};
+
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+); // 400
