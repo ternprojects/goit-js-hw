1df335bf5ad9550ffb994b693082dcf3b9646c7e
Yuriy <ternxstyle@gmail.com>
2020-05-21 23:37:27 +0300

    add 1-st task

--- module-03/scripts.js
+++ module-03/scripts.js
@@ -1,7 +1,7 @@
-
-import "./tasks/task-01.js";
+//import "./tasks/task-01.js";
 //import "./tasks/task-02.js";
 //import "./tasks/task-03.js";
 //import "./tasks/task-04.js";
-//import "./tasks/task-05.js"; 
+//import "./tasks/task-05.js";
 //import "./tasks/task-06.js";
+
---
+++ module-04/scripts.js
@@ -0,0 +1,4 @@
+import "./tasks/task-01.js";
+//import "./tasks/task-02.js";
+
+
