8376f3d781f08c747bfe4b007cb6fffe76a15741
Yuriy <ternxstyle@gmail.com>
2020-05-22 01:59:19 +0300

    add 2 task

--- module-03/scripts.js
+++ module-03/scripts.js
@@ -1,4 +1,4 @@
-//import "./tasks/task-01.js";
+import "./tasks/task-01.js";
 //import "./tasks/task-02.js";
 //import "./tasks/task-03.js";
 //import "./tasks/task-04.js";
--- module-04/tasks/task-02.js
+++ module-04/tasks/task-02.js
@@ -1,2 +1,34 @@
 'use strict';
 
+debugger;
+
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+
+  console.log(`Adding ${itemName} to inventory`);
+  this.items.push(itemName);
+  },
+
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+    console.log(`Removing ${itemName} from inventory`);
+  },
+};
+
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
+
+invokeInventoryAction('Medkit', inventory.add.bind(inventory));
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
