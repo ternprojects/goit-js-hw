95aaf49a799fefdfa40b6b292b9a018e8b5a4cb9
Yuriy <ternxstyle@gmail.com>
2020-05-26 00:01:55 +0300

    add 2 task

--- module-05/tasks/task-02.js
+++ module-05/tasks/task-02.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const User = function ({ name, age, followers }) {
+  this.name = name;
+  this.age = age;
+  this.followers = followers;
+
+  this.getInfo = function() {
+    console.log(`User ${name} is ${age} years old and has ${followers} followers`);
+  }
+}
+
+const mango = new User({
+  name: 'Mango',
+  age: 2,
+  followers: 20,
+});
+
+mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+const poly = new User({
+  name: 'Poly',
+  age: 3,
+  followers: 17,
+});
+
+poly.getInfo(); // User Poly is 3 years old and has 17 followers
