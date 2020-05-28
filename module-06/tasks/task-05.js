0e190b1b865ed8334d5286805b521240b619055d
Yuriy <ternxstyle@gmail.com>
2020-05-29 01:41:24 +0300

    module 06 - added fifth task

--- module-06/tasks/task-05.js
+++ module-06/tasks/task-05.js
@@ -3,7 +3,7 @@
 import users from './users.js';
 
 const getUserWithEmail = (users, email) => {
-  // твой код
+  return users.find(user => user.email === email)
 };
 
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
