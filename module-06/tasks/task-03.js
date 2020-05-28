e4a18be201d8d301dfa6050ed1e97a57f441b42c
Yuriy <ternxstyle@gmail.com>
2020-05-29 01:21:12 +0300

    madule 6 - added third task

--- module-06/tasks/task-03.js
+++ module-06/tasks/task-03.js
@@ -3,7 +3,7 @@
 import users from './users.js';
 
 const getUsersWithGender = (users, gender) => {
-  // твой код
+   return users.filter(user => user.gender === gender);
 };
 
 console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
