b05b63e0754cacd713536f90f9ea9e1bc65ee048
Yuriy <ternxstyle@gmail.com>
2020-05-29 01:27:38 +0300

    module 6 - added fourth task

--- module-06/scripts.js
+++ module-06/scripts.js
@@ -3,4 +3,10 @@ import "./tasks/task-01.js";
 //import "./tasks/task-03.js";
 //import "./tasks/task-04.js";
 //import "./tasks/task-05.js";
+//import "./tasks/task-06.js";
+//import "./tasks/task-07.js";
+//import "./tasks/task-08.js";
+//import "./tasks/task-09.js";
+//import "./tasks/task-10.js";
+
 
