8c5e0a7aef6bd2ad738f931d0ce181f43f6c6d5a
Yuriy <ternxstyle@gmail.com>
2020-05-31 02:40:44 +0300

    change code in 6-th module

--- module-06/tasks/tasks.js
+++ module-06/tasks/tasks.js
@@ -4,11 +4,11 @@
 
 import users from './users.js';
 
-//const getUserNames = users => {
-//  return users.map(user => user.name);
-//};
+const getUserNames = users => {
+  return users.map(user => user.name);
+};
 
-//console.log(getUserNames(users));
+console.log(getUserNames(users));
 
 //[ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 
