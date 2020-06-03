f629c9aac52cadb01c9cf900975bf6c4847f46fe
Yuriy <ternxstyle@gmail.com>
2020-06-03 05:33:52 +0300

    module 7, added 5-th task

--- module-07/script.js
+++ module-07/script.js
@@ -2,8 +2,6 @@ import "./tasks/task_1.js";
 //import "./tasks/task_2.js";
 //import "./tasks/task_3.js";
 //import "./tasks/task_4.js";
-//import "./tasks/task-05.js";
-//import "./tasks/task-06.js";
-//import "./tasks/task-07.js";
-//import "./tasks/task-08.js";
-
+//import "./tasks/task_5.js";
+//import "./tasks/task_6.js";
+//import "./tasks/task_7.js";
--- module-07/tasks/task_5.js
+++ module-07/tasks/task_5.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const inputText = document.querySelector('#name-input');
+const outputText = document.querySelector('#name-output');
+
+inputText.addEventListener('input', inputChangeText);
+
+function inputChangeText(e) {
+  outputText.textContent = e.target.value;
+
+  if (outputText.textContent === '') {
+    outputText.textContent = 'незнакомец';
+  }
+} 
+
