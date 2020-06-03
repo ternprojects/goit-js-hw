46dc03e33de1c16383ce38b59c9e2085a5c44b0a
Yuriy <ternxstyle@gmail.com>
2020-06-03 05:11:34 +0300

    module 7, added 4 task

--- module-07/script.js
+++ module-07/script.js
@@ -1,7 +1,7 @@
-//import "./tasks/task_1.js";
+import "./tasks/task_1.js";
 //import "./tasks/task_2.js";
-import "./tasks/task_3.js";
-//import "./tasks/task-04.js";
+//import "./tasks/task_3.js";
+//import "./tasks/task_4.js";
 //import "./tasks/task-05.js";
 //import "./tasks/task-06.js";
 //import "./tasks/task-07.js";
--- module-07/tasks/task_4.js
+++ module-07/tasks/task_4.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const counterValue = document.querySelector('#value');
+
+const counterIncrement = document.querySelector('button[data-action="increment"]');
+const counterDecrement = document.querySelector('button[data-action="decrement"]');
+
+counterIncrement.addEventListener('click', increment);
+counterDecrement.addEventListener('click', decrement);
+
+function increment() {
+  counterValue.textContent = Number(counterValue.textContent) + 1;
+}
+
+function decrement() {
+  counterValue.textContent -= 1;
+}
+
+
