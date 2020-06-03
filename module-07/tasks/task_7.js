8a44b6215053db986d727ea3ebe967c953b9556b
Yuriy <ternxstyle@gmail.com>
2020-06-03 07:35:07 +0300

    module 7, added 7-th task

--- module-07/tasks/task_7.js
+++ module-07/tasks/task_7.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const inputText = document.querySelector('#font-size-control');
+const spanText = document.querySelector('#text');
+
+inputText.addEventListener('input', changeFontSize);
+
+function changeFontSize() {
+  spanText.style.fontSize = `${inputText.value}px`;
+}
