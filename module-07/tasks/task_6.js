4d080556f1711de0c719fa998f1e1957705a9d71
Yuriy <ternxstyle@gmail.com>
2020-06-03 06:26:47 +0300

    module 7, added 6-th task

--- module-07/tasks/task_6.js
+++ module-07/tasks/task_6.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const inputText = document.querySelector('#validation-input');
+const lengthValue = inputText.dataset.length;
+
+inputText.addEventListener('blur', blurOn);
+
+function blurOn() {
+
+  if (inputText.value.length >= lengthValue) {
+    inputText.classList.remove('invalid');
+    return inputText.classList.add('valid');
+  }
+
+  if (inputText.value.length < lengthValue) {
+    inputText.classList.remove('valid');
+    return inputText.classList.add('invalid');
+  }
+}
