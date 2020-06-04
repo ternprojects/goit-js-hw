73fe7af745f2b7ba9f3d0b555d66ac396be9a348
Yuriy <ternxstyle@gmail.com>
2020-06-05 00:49:36 +0300

    module 8, create open img

--- module-08/js/script.js
+++ module-08/js/script.js
@@ -4,6 +4,11 @@ import { tempImg } from './template.js';
 const list = document.querySelector('.js-gallery');
 const allImg = dataImg.map(item => tempImg(item)).join('');
 
-/* console.log(allImg); */
-
 list.insertAdjacentHTML('beforeend', allImg);
+list.addEventListener('click', clickList)
+
+function clickList(e) {
+  if (e.target.nodeName !== "IMG") return;
+  const { source } = e.target.dataset;
+  e.target.src = source;
+}
--- module-08/js/template.js
+++ module-08/js/template.js
@@ -1,9 +1,17 @@
 export const tempImg = item => {
   return `
-  <li>
-    <img src="${item.preview}"
-    data-source = "${item.original}"
-    alt="${item.description}">
+  <li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${item.original}"
+    >
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
   </li>
-  `;
+`;
 }
